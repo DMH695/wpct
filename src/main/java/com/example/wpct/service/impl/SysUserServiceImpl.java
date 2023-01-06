42e6f8466cf6a443fb164874a94d2425e0a03ef3
FT-Fetters <1438255191@qq.com>
2023-01-06 18:24:26 +0800

    fixed: 调整项目结构

--- src/main/java/com/example/wpct/service/impl/SysUserServiceImpl.java
+++ src/main/java/com/example/wpct/service/impl/SysUserServiceImpl.java
@@ -1,12 +1,13 @@
-package com.example.wpct.service;
+package com.example.wpct.service.impl;
 
 import com.example.wpct.mapper.SysUserMapper;
 import com.example.wpct.entity.SysUser;
+import com.example.wpct.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SysUserServiceImpl implements SysUserService{
+public class SysUserServiceImpl implements SysUserService {
     @Autowired
     SysUserMapper sysUserMapper;
 
