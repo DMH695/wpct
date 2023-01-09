d6336ea2ab360d3ba0f3ab1e098e998f0290c3e9
Rossland <1145902985@qq.com>
2023-01-09 12:47:14 +0800

    WechatUser实体类

---
+++ src/main/java/com/example/wpct/entity/WechatUser.java
@@ -0,0 +1,59 @@
+package com.example.wpct.entity;
+
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigInteger;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("wechat_user")
+public class WechatUser {
+
+    /**
+     * 主键id
+     */
+    private BigInteger id;
+
+    /**
+     * 微信用户昵称
+     */
+    private String nickName;
+
+    /**
+     * 微信用户姓名
+     */
+    private String name;
+
+    /**
+     * 电话号码
+     */
+    private String telephone;
+
+    /**
+     * openid
+     */
+    private Character openid;
+
+    /**
+     * 小区id (village_id)
+     */
+    private BigInteger vid;
+
+    /**
+     * 楼栋id (build_id)
+     */
+    private BigInteger bid;
+
+    /**
+     * 房号
+     */
+    private BigInteger rid;
+
+
+
+}
