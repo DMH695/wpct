68c8b5434250c7541692f58bd1c55f6e3519afd5
aykls <1781954235@qq.com>
2023-01-10 09:21:17 +0800

    意见管理

---
+++ src/main/java/com/example/wpct/controller/ExamineController.java
@@ -0,0 +1,45 @@
+package com.example.wpct.controller;
+import com.example.wpct.entity.ExamineDto;
+import com.example.wpct.service.impl.ExamineServiceImpl;
+import com.example.wpct.utils.ResultBody;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Api(tags = "意见处理")
+@CrossOrigin
+@RequestMapping("/examine/info")
+public class ExamineController {
+    @Autowired
+    private ExamineServiceImpl examineService;
+
+
+    @ApiOperation("新增处理（审批）")
+    @PostMapping("/add")
+    public ResultBody addExamine(@RequestParam String openid,@RequestParam String examineContent) {
+        return examineService.addExamine(openid,examineContent);
+    }
+    @ApiOperation("后台处理（意见,审批）列表")
+    @GetMapping("/list")
+    public ResultBody listExamine(@RequestParam int pageNum,@RequestParam int pageSize) {
+        return ResultBody.ok(examineService.listExamine(pageNum,pageSize));
+    }
+
+    @ApiOperation("微信用户历史 记录处理（审批）")
+    @PostMapping("/wxlist")
+    public ResultBody userExamine(@RequestBody String openid) {
+        return examineService.userExamineList(openid);
+    }
+
+
+    /**
+     * 后台管理员处理用户意见
+     */
+    @ApiOperation("处理意见/审批")
+    @PostMapping("/solu")
+    public ResultBody examineHandle(@RequestParam Integer id,@RequestParam String openid,@RequestParam(required = false) String resolveMsg) {
+        return examineService.examineHandle(id, openid, resolveMsg);
+    }
+}
---
+++ src/main/java/com/example/wpct/service/ExamineService.java
@@ -0,0 +1,23 @@
+package com.example.wpct.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.wpct.entity.ExamineDto;
+import com.example.wpct.utils.ResultBody;
+import com.example.wpct.utils.page.PageResult;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * @Author ZXX
+ * @InterfaceName ExamineService
+ * @Description TODO
+ * @DATE 2022/10/10 17:03
+ */
+@Service
+public interface ExamineService extends IService<ExamineDto> {
+    ResultBody addExamine(String openid,String examineContent);
+    PageResult listExamine(int pageNum, int pageSize);
+    ResultBody examineHandle(Integer id, String openid,String resolveMsg);
+    ResultBody userExamineList(String openid);
+
+}
